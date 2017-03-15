da42a4a6f932fdf77dfd726c0349c822a4eb6633
Etiene Dalcol <dalcol@etiene.net>
2017-03-15 17:05:04 +0000

    refactor

--- src/openpr/core.clj
+++ src/openpr/core.clj
@@ -7,24 +7,20 @@
             [clj-http.client :as client]
             [environ.core :refer [env]]))
 
+(defn- github-fetch [& params]
+  ((client/get (str "https://api.github.com/" (apply str params))
+    {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body))
+
 (defn- get-team-id [org team]
-  (let [teams
-    ((client/get (str "https://api.github.com/orgs/" org "/teams")
-        {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body)]
+  (let [teams (github-fetch "orgs/" org "/teams")]
     ((first(filter #(= (% :name) team) teams)) :id)))
 
 (defn- get-members [org team]
-  ((client/get (str "https://api.github.com/teams/" (get-team-id org team) "/members")
-      {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body))
-
-(defn- get-repo-prs [org repo]
-  ((client/get (str "https://api.github.com/repos/" org "/" repo "/pulls")
-      {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body))
+  (github-fetch "teams/" (get-team-id org team) "/members"))
 
 (defn- get-org-prs [org team]
-  (let [repos ((client/get (str "https://api.github.com/orgs/" org "/repos")
-      {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body)]
-    (concat (map #( get-repo-prs org (% :name) ) repos))))
+  (let [repos (github-fetch "orgs/" org "/repos")]
+    (concat (map #(github-fetch "repos/" org "/" (% :name) "/pulls") repos))))
 
 (defroutes api-routes
   (GET "/:org/:team" [org team] {:body (get-org-prs org team)})
