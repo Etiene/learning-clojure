fb37abb34b963ad632b70db65c0bf1519b6b053e
Etiene Dalcol <dalcol@etiene.net>
2017-03-15 16:51:12 +0000

    Get PRs from an org

--- src/openpr/core.clj
+++ src/openpr/core.clj
@@ -7,15 +7,27 @@
             [clj-http.client :as client]
             [environ.core :refer [env]]))
 
-(defn get-team-id [org team]
-  (let [res
+(defn- get-team-id [org team]
+  (let [teams
     ((client/get (str "https://api.github.com/orgs/" org "/teams")
-        {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto})
-          :body)]
-    ((first(filter #(= (% :name) team) res)) :id)))
+        {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body)]
+    ((first(filter #(= (% :name) team) teams)) :id)))
+
+(defn- get-members [org team]
+  ((client/get (str "https://api.github.com/teams/" (get-team-id org team) "/members")
+      {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body))
+
+(defn- get-repo-prs [org repo]
+  ((client/get (str "https://api.github.com/repos/" org "/" repo "/pulls")
+      {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body))
+
+(defn- get-org-prs [org team]
+  (let [repos ((client/get (str "https://api.github.com/orgs/" org "/repos")
+      {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body)]
+    (concat (map #( get-repo-prs org (% :name) ) repos))))
 
 (defroutes api-routes
-  (GET "/:org/:team" [org team] {:body {:id (get-team-id org team)}})
+  (GET "/:org/:team" [org team] {:body (get-org-prs org team)})
   (route/not-found {:body {:error "Page not found"}}))
 
 (def api
