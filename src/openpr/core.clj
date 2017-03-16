25923e04813eb567ae84db2ee6fe5c925b09a9c6
Etiene Dalcol <dalcol@etiene.net>
2017-03-16 14:44:36 +0000

    Refactor

--- src/openpr/core.clj
+++ src/openpr/core.clj
@@ -5,22 +5,23 @@
             [ring.middleware.json :as json]
             [ring.middleware.params :as params]
             [clj-http.client :as client]
+            [clojure.string :refer [join]]
             [environ.core :refer [env]]))
 
 (defn- github-fetch [& params]
-  ((client/get (str "https://api.github.com/" (apply str params))
+  ((client/get (str "https://api.github.com/" (join "/" params))
     {:basic-auth [(env :github-user) (env :github-pass)] :accept :json :as :auto}) :body))
 
 (defn- get-team-id [org team]
-  (let [teams (github-fetch "orgs/" org "/teams")]
+  (let [teams (github-fetch "orgs" org "teams")]
     ((first (filter #(= (% :name) team) teams)) :id)))
 
 (defn- get-member-logins [org team]
-  (map #(% :login) (github-fetch "teams/" (get-team-id org team) "/members")))
+  (map #(% :login) (github-fetch "teams" (get-team-id org team) "members")))
 
 (defn- get-org-prs [org]
-  (let [repos (github-fetch "orgs/" org "/repos")]
-    (apply concat (map #(github-fetch "repos/" org "/" (% :name) "/pulls") repos))))
+  (let [repos (github-fetch "orgs" org "repos")]
+    (apply concat (map #(github-fetch "repos" org (% :name) "pulls") repos))))
 
 (defn- get-team-prs [org team]
   (let [prs (get-org-prs org) member-logins (get-member-logins org team)]
